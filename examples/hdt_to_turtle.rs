adb306d023034ef66f7758a30751d51e486f3a77
Jos van den Oever <jos@vandenoever.info>
2017-01-22 18:11:05 +0100

    Add some namespaces to the example

--- examples/hdt_to_turtle.rs
+++ examples/hdt_to_turtle.rs
@@ -5,11 +5,25 @@ use std::env::args;
 use rdfio::namespaces::Namespaces;
 
 fn main() {
-    use rdfio::graph::{Graph, Triple, LiteralPtr};
+    use rdfio::graph::{Graph};
     let mut args = args();
-    let exe = args.next().unwrap();
+    args.next();
     let hdt = args.next().unwrap();
     let hdt = rdfio_hdt::HDT::new(&hdt).unwrap();
-    let namespaces = Namespaces::new();
-    rdfio::io::write_turtle(&namespaces, hdt.iter(), &mut ::std::io::stdout());
+    let mut namespaces = Namespaces::new();
+    namespaces.set(b"dc", "http://purl.org/dc/terms/");
+    namespaces.set(b"foaf", "http://xmlns.com/foaf/0.1/");
+    namespaces.set(b"geo", "http://www.w3.org/2003/01/geo/");
+    namespaces.set(b"l", "http://www.monnet-project.eu/lemon#");
+    namespaces.set(b"o", "http://dbpedia.org/ontology/");
+    namespaces.set(b"owl", "http://www.w3.org/2002/07/owl#");
+    namespaces.set(b"prov", "http://www.w3.org/ns/prov#");
+    namespaces.set(b"r", "http://dbpedia.org/resource/");
+    namespaces.set(b"rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+    namespaces.set(b"s", "http://schema.org/");
+    namespaces.set(b"skos", "http://www.w3.org/2004/02/skos/core#");
+    namespaces.set(b"wkr", "http://wiktionary.dbpedia.org/resource/");
+    namespaces.set(b"wkt", "http://wiktionary.dbpedia.org/terms/");
+    namespaces.set(b"xs", "http://www.w3.org/2001/XMLSchema#");
+    rdfio::io::write_turtle(&namespaces, hdt.iter(), &mut ::std::io::stdout()).expect("failed to write turtle");
 }
